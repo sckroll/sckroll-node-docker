cc0a70cd34f2563f89fad648dc6035c5dc863ce8
sckroll <kimsc0714@gmail.com>
2021-05-18 17:20:26 +0900

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,60 @@
+const User = require('../models/userModel')
+const bcrypt = require('bcryptjs')
+
+exports.signUp = async (req, res, next) => {
+  const { username, password } = req.body
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12)
+    const newUser = await User.create({
+      username,
+      password: hashedPassword
+    })
+    req.session.user = newUser
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: newUser
+      }
+    })
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({
+      status: 'fail'
+    })
+  }
+}
+
+exports.login = async (req, res, next) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await User.findOne({ username })
+
+    if (!user) {
+      res.status(404).json({
+        status: 'fail',
+        message: 'user not found'
+      })
+    }
+
+    const isCorrect = await bcrypt.compare(password, user.password)
+
+    if (isCorrect) {
+      req.session.user = user
+      res.status(201).json({
+        status: 'success'
+      })
+    } else {
+      res.status(400).json({
+        status: 'fail',
+        message: 'incorrect username or password'
+      })
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({
+      status: 'fail'
+    })
+  }
+}
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const redis = require('redis')
+const cors = require('cors')
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, REDIS_URL, REDIS_PORT, SESSION_SECRET } = require('./config/config')
+const postRouter = require('./routes/postRoutes')
+const userRouter = require('./routes/userRoutes')
+
+// 레디스 세션 설정
+let RedisStore = require('connect-redis')(session)
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT
+})
+
+const app = express()
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+    .then(() => console.log('successfully connected to DB'))
+    .catch(e => {
+      console.error(e)
+      // 5초 후 서버 재연결 시도
+      setTimeout(connectWithRetry, 5000)
+    })
+}
+connectWithRetry()
+
+app.enable('trust proxy')
+app.use(cors({}))
+app.use(session({
+  store: new RedisStore({ client: redisClient }),
+  secret: SESSION_SECRET,
+  cookie: {
+    secure: false,
+    resave: false,
+    saveUninitialized: true,
+    httpOnly: true,
+    maxAge: 1000 * 60
+  }
+}))
+
+app.use(express.json())
+
+// 라우트 설정
+app.get('/api/v1', (req, res) => {
+  res.send('<h2>Sckroll</h2>')
+  console.log('hell yeah')
+})
+app.use('/api/v1/posts', postRouter)
+app.use('/api/v1/users', userRouter)
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+})
